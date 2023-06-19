573853d01a46b479917936191e1dadb6307bfee6
Faizan Sayyed <faizansayyed36@gmail.com>
2023-06-19 11:49:50 +0530

    graph ql stater code

---
+++ server/index.js
@@ -0,0 +1,53 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+  
+  // Resolvers define the technique for fetching the types defined in the
+  // schema. This resolver retrieves books from the "books" array above.
+  const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+// Note you must call `start()` on the `ApolloServer`
+// instance before passing the instance to `expressMiddleware`
+await server.start();
+
+// Specify the path where we'd like to mount our server
+app.use('/graphql', cors(), expressMiddleware(server));
